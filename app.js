dbca61c749db03c515a138d1e1d4cdd2664c332f
trisha3290 <88582905+trisha3290@users.noreply.github.com>
2022-12-26 15:59:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 app.set('view engine', 'ejs'); 
 app.set('views','views');
-app.use(express.static('public'));// includes css files and js files which are embedded in html 
+app.use(express.static('/public'));// includes css files and js files which are embedded in html 
 app.use(express.json());
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
